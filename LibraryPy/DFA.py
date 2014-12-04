1d962a8cab2e86db99aa122a70f6392d5d73f02f
Feng Ruohang <fengruohang@gmail.com>
2014-12-04 09:25:54 +0800

    Change on Status Machine

--- LibraryPy/DFA.py
+++ LibraryPy/DFA.py
@@ -1,3 +1,5 @@
+
+'''Self Define DFA Dead Exception'''
 class DFADeadException(Exception):
     def __init__(self,cnt,q,input):
         Exception.__init__(self)
@@ -8,12 +10,14 @@ class DFADeadException(Exception):
 
 '''A DFA takes five arguments: StateSet Alphabet TransformFunc InitialState FinalState'''
 class DFA(object):
-    def __init__(self,Q,Sigma,d,q0,F):
-        self.Q = Q           #Init State Q: A Set of possible state
-        self.Sigma = Sigma       #Init Alphabet Sigma: A Set of Character
-        self.d = d           #Init TransFunction delta: A callable object
-        self.q = q0          #Init InitState q0: q0 in Q should be true
-        self.F = F           #Init Final States: F should be contained in Q
+    def __init__(self,Q,S,T,q0,F):
+        self.Q = Q          #Init State Q: A Set of possible state
+        self.S = S          #Init Alphabet Sigma S: A Set of Character
+        self.T = T          #Init TransFunction T: A callable object
+        self.q0 = q0        #Init InitState q0: q0 in Q should be true
+        self.F = F          #Init Final States: F should be contained in Q
+        self.q = q0         #Init DFA Current State q with q0.
+
 
         if not q0 in Q:
             raise ValueError("Init State q0 is not in Available State Set Q")
@@ -22,21 +26,29 @@ class DFA(object):
             raise ValueError("Final State Set F is not a subset of State Set Q")
 
 
-    def __call__(self,string):
-        for cnt,letter in enumerate(string):
-            newState = self.d(self.q,letter)
-            infoString = "%d:\t[%s] -- (%s) --> [%s]" % (cnt,self.q,letter,newState)
+    '''Judge Whether a string belongs to this DFA's Language'''
+    def __call__(self,stringInput):
+        self.q = self.q0        #Reset before start
+        for cnt,e in enumerate(stringInput):
+            oldState = self.q
+            newState = self.T[oldState][e]
+            infoString = "%d:\t[%s] -- (%s) --> [%s]" % (cnt,oldState,e,newState)
             print infoString
             if not newState:
-                raise DFADeadException(cnt,self.q,letter)
+                raise DFADeadException(cnt,oldState,cnt)
             self.q = newState
         return True if self.q in self.F else False
 
     
     '''Cartesian Product of two DFA'''
     def mul(self,other):
-        newQ =  {(i,j) for i in self.Q for j in other.Q}
-        newSigma
+        newQ =  {(i,j) for i in self.Q for j in other.Q}    #Cartesian Product of State Set
+        newS = self.S.union(other.S)            #Union of factors
+        newd = { {} for i in newQ for j in newQ}
+                
+        newq0 = (self.q0,other.q0);
+        newF = {(i,j) for i in self.F for j in other.F}    #Cartesian Product of Final State Set
+
 
 
 if __name__ == "__main__":
@@ -44,12 +56,12 @@ if __name__ == "__main__":
 
     state = {'A','B','C','D'}
     sigma = {0,1,2}
-    transf = lambda q,a:{
+    transf = {
       'A':{0:'C',1:'A',2:'D'},
       'B':{0:'A',1:'B',2:None},
       'C':{0:'B',1:'B',2:None},
       'D':{0:'D',1:'D',2:None}
-        }[q][a]
+        }
     q0 = 'B'
     F = {'D'}
 
